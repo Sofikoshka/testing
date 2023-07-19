e3e3bed5d7e19bd7b9b29eb0aa05fbf2e1c3db3b
Sofikoshka <sofiia.kolokolcheva@nure.ua>
2023-07-19 19:36:28 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testing';
+  password: string = '';
+  strengthSections: string[] = Array(3).fill('gray');
+
+  updateStrength() {
+    if (this.password.length === 0) {
+      this.strengthSections.fill('gray');
+    } else if (this.password.length < 8) {
+      this.strengthSections.fill('red');
+    } else if (this.password.match(/^[a-zA-Z]+$/) || this.password.match(/^[0-9]+$/) || this.password.match(/^[\W_]+$/))  {
+      this.strengthSections[0] = 'red';
+      this.strengthSections[1] = 'gray';
+      this.strengthSections[2] = 'gray';
+    } else if (
+      this.password.match(
+        /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).+$/
+      )
+    ) {
+      this.strengthSections.fill('green');
+     
+    } else {
+      this.strengthSections[0] = 'yellow';
+      this.strengthSections[1] = 'yellow';
+      this.strengthSections[2] = 'gray';
+    }
+  }
 }
