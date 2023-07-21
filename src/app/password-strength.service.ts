a8df32e95102a4e6b6e2f9fe662262d87e1afbac
Sofikoshka <sofiia.kolokolcheva@nure.ua>
2023-07-21 18:29:26 +0300

    done

--- src/app/password-strength.service.ts
+++ src/app/password-strength.service.ts
@@ -5,19 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class PasswordStrengthService {
   getStrength(password: string): string[] {
+    const gray = 'gray';
+    const red = 'red';
+    const yellow = 'yellow';
+    const green = 'green';
+  
     if (password.length === 0) {
-        return ['gray', 'gray', 'gray'];
-      } else if (password.length < 8) {
-        return ['red', 'red', 'red'];
-      }  else if (password.match(/^[a-zA-Z]+$/) || password.match(/^[0-9]+$/) || password.match(/^[\W_]+$/)) {
-        return ['red', 'gray', 'gray'];
-      } else if (password.match(
-        /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).+$/
-      )
-    )  {
-        return ['green', 'green', 'green'];
-      } else {
-        return ['yellow', 'yellow', 'gray'];
-      }
+      return [gray, gray, gray];
+    }
+  
+    const hasLetters = /[a-zA-Z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSymbols = /[\W_]/.test(password);
+  
+    if (password.length < 8 || (!hasLetters && !hasNumbers && !hasSymbols)) {
+      return [red, red, red];
+    } else if (hasLetters && hasNumbers && hasSymbols) {
+      return [green, green, green];
+    } else if (hasLetters && hasNumbers) {
+      return [yellow, yellow, gray];
+    } else if (hasLetters && hasSymbols) {
+      return [yellow,  yellow, gray];
+    } else if (hasNumbers && hasSymbols) {
+      return [ yellow, yellow, gray];
+    } else if (hasLetters || hasNumbers || hasSymbols) {
+      return [red, gray, gray];
+    }
+  
+    return [gray, gray, gray];
   }
+  
+  
 }
